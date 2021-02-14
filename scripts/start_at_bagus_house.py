c721e075442dad91d56dbef2e71311e3213e8533
Chris Frantz <frantzcj@gmail.com>
2021-02-14 10:33:47 -0800

    Start inside Bagu's House.
    Set up the door stack so you'll exit the house and appear at the
    doorway.

--- scripts/start_at_bagus_house.py
+++ scripts/start_at_bagus_house.py
@@ -24,9 +24,36 @@ def hack(config, edit, asm):
         STA $070a
         STA $0706           ; Region
         STA $056c           ; Palace code
+
+        ; Door stack parameters:
+        ; I don't understand what any of these values mean (except $075b).
+        ; I just took the values from NES RAM after performing the door
+        ; transition I wanted.
+        ldx #5
+        sta $69b3,x
+        sta $69ac,x
+        sta $69a5,x
+        sta $05cc,x
+        lda #1
+        sta $075b           ; Door stack depth?
+        sta $69ba,x
+        sta $6990,x
+        lda #3
+        sta $6989,x
+        sta $697b,x
+        lda #6
+        sta $699e,x
+        lda #10
+        sta $6982,x
+        lda #11
+        sta $6997,x
+        lda #$60
+        sta $05d3,x
+
+        lda #8
+        sta $079a           ; Have Bagu's note (cuz you ARE Bagu).
+
     setup_done:
         RTS
-
-
     """)
     return config
